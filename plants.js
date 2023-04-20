2ed6f5db568f1171c278a8b8014cb4395346e9e1
Michelle Villagran <mrvillagran16@gmail.com>
2023-04-19 18:04:48 -0600

    Add code to print plant conditional

--- plants.js
+++ plants.js
@@ -1,3 +1,21 @@
 var getsSunshine = true
 var ouncesOfWater = 10
 
+if (getsSunshine === true && ouncesOfWater >= 10){
+    console.log("Plant will thrive!");
+}
+   else if (getsSunshine === false || ouncesOfWater < 10){
+    console.log("Plant will die!")
+   }
+
+
+   var getsSunshine = true
+   var ouncesOfWater = 1
+   
+   if (getsSunshine === true && ouncesOfWater >= 10){
+       console.log("Plant will thrive!");
+   }
+      else if (getsSunshine === false || ouncesOfWater < 10){
+       console.log("Plant will die!")
+      }
+   
